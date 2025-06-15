59354cc556f0e7c04d42b1ed28bfb7e8da0e6ea6
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-15 02:45:45 -0500

    added hostname in output

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "vm_ip" {
   value = esxi_guest.vm.ip_address
   description = "IP address assigned to the VM"
 }
+
+output "vm_hostname" {
+  value = var.vm_hostname
+  description = "Hostname assigned to the VM"
+}
