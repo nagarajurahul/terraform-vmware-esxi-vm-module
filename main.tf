e773b0b8227a94ec9c37169740ff949142915ad2
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-06-16 20:26:28 -0500

    Update main.tf - to remove providers

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_version = ">= 0.12"
-}
-
-provider "esxi" {
-  esxi_hostname = var.esxi_hostname
-  esxi_hostport = var.esxi_hostport
-  esxi_hostssl  = var.esxi_hostssl
-  esxi_username = var.esxi_username
-  esxi_password = var.esxi_password
-}
-
 data "template_file" "userdata_default" {
   template = file("userdata.tpl")
   vars = {
